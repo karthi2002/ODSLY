be1779fd21159804fbf6d8a1ee15c5b944c6e1d3
karthi srinivasan <94228520+karthi2002@users.noreply.github.com>
2025-04-08 13:46:52 +0530

    third commit

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import colors from "../utils/Colors";
 import {
   View,
   Text,
@@ -10,6 +11,8 @@ import {
 } from "react-native";
 
 import Logo from "../assets/icons/Logo.png";
+import Google from "../assets/icons/google.png";
+import Apple from "../assets/icons/apple.png";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -21,7 +24,6 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
-      
       <View style={styles.logoContainer}>
         <Image source={Logo} style={styles.logo} />
         <Text style={styles.logoText}>ODSLY</Text>
@@ -47,15 +49,44 @@ const LoginScreen = () => {
           style={styles.input}
         />
 
-        <TouchableOpacity>
-          <Text style={styles.link}>Forgot password?</Text>
-        </TouchableOpacity>
+        <View style={styles.forgotContainer}>
+          <TouchableOpacity>
+            <Text style={styles.link}>Forgot password?</Text>
+          </TouchableOpacity>
+        </View>
 
         <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
           <Text style={styles.loginText}>Login</Text>
         </TouchableOpacity>
 
-        <Text style={styles.loginWith}>Login with</Text>
+        <View style={styles.loginWithContainer}>
+          <View style={styles.line} />
+          <Text style={styles.loginWith}>Login with</Text>
+          <View style={styles.line} />
+        </View>
+
+        <View style={styles.authContainer}>
+          <TouchableOpacity>
+            <Image style={styles.authLogo} source={Google} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.authLogo} source={Apple} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.loginWithContainer}>
+          <View style={styles.line} />
+          <Text style={styles.loginWith}>OR</Text>
+          <View style={styles.line} />
+        </View>
+
+        <TouchableOpacity>
+          <Text style={styles.dontaccount}>Don't have a Account?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.singup}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -64,7 +95,7 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#222",
+    backgroundColor: colors.background,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -73,7 +104,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: 10,
     alignItems: "center",
-    marginBottom: 30,
+    marginBottom: 40,
   },
   logo: {
     width: 50,
@@ -81,54 +112,107 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   logoText: {
-    color: "#FFF",
-    fontSize: 32,
+    color: colors.secondary,
+    fontSize: 42,
     fontWeight: "bold",
   },
   loginBox: {
-    backgroundColor: "#FFF",
+    backgroundColor: colors.secondary,
     borderRadius: 10,
-    padding: 20,
-    width: "80%",
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent:'center',
-    alignItems:'center'
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    width: "85%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 26,
+    marginBottom: 30,
+    color: colors.background,
     fontWeight: "bold",
   },
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
-    marginBottom: 15,
-    width: '100%',
-    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    width: "100%",
+    color: colors.text,
+    paddingVertical: 12,
     paddingHorizontal: 10,
+  },
+  forgotContainer: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginTop: -8
   },
   link: {
     color: "#000",
     marginBottom: 20,
+    fontSize: 16,
+    fontWeight: "600",
+    color: colors.background,
+    textDecorationLine: "underline",
     textAlign: "right",
   },
   loginButton: {
     backgroundColor: "#007BFF",
-    padding: 12,
-    width: '100%',
+    paddingVertical: 15,
+    width: "100%",
     borderRadius: 8,
     alignItems: "center",
-    marginBottom: 15,
+    marginVertical: 10,
   },
   loginText: {
-    color: "#FFF",
-    fontWeight: "bold",
+    color: colors.secondary,
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  loginWithContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#ccc",
+    marginHorizontal: 10,
   },
   loginWith: {
-    textAlign: "center",
-    color: "#555",
+    color: colors.text,
+    fontSize: 16,
+    fontWeight: 500,
   },
+  authContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+  },
+  authLogo: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+  },
+  dontaccount: {
+    color: colors.text,
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  singup :{
+    color: colors.background,
+    fontSize: 18,
+    fontWeight: 600,
+    textDecorationLine: 'underline',
+    marginTop: 2
+  }
 });
 
 export default LoginScreen;
---
+++ utils/Colors.jsx
@@ -0,0 +1,11 @@
+const Colors = {
+    primary: '#000000',
+    secondary: '#FFFFFF',
+    success: '#01884B',
+    error: '#DC143C',
+    background: '#000A34',
+    text: '#667085',
+  };
+  
+export default Colors;
+  
