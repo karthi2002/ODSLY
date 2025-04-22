308f6021fdb9bf7d4f637eb678325a210bcfa1a5
madhan1204 <madhankumar1204@outlook.com>
2025-04-22 19:09:01 +0530

    Changes made

--- screens/community/FeedScreen.jsx
+++ screens/community/FeedScreen.jsx
@@ -8,8 +8,16 @@ import {
 } from "react-native";
 import Colors from "../../utils/Colors";
 import { LineGradient } from "../../layouts/LineGradient";
-import { recentBet } from "../../json/RecentBetData";
-import BetCard from "../../components/Card/BetCard";
+import Tag from "../../components/List/Tag";
+import UserPostCard from "../../components/Card/UserPostCard";
+
+
+const tagsData = [
+  { label: "#NBAWinners", emoji: "🏀", selected: true },
+  { label: "#ParlayKings", emoji: "🔥", selected: false },
+  { label: "#BigWins", emoji: "💰", selected: false },
+  { label: "#UnderdogBets", emoji: "⚽", selected: true },
+];
 
 export default function FeedScreen() {
   const handleAvatarPress = () => {
@@ -32,9 +40,39 @@ export default function FeedScreen() {
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
       >
+      <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Trending Hashtags</Text>
+              <View style={styles.container}>
+                {tagsData.map((tag, index) => (
+                  <Tag
+                    key={index}
+                    label={tag.label}
+                    emoji={tag.emoji}
+                    selected={tag.selected}
+                  />
+                ))}
+            </View>
+            </View>
+            <LineGradient />
 
-
-
+            <View>
+            <Text style={styles.sectionTitle}>Trending Posts</Text>
+              <ScrollView  showsVerticalScrollIndicator={false} style={{ marginRight: -15 }} ><UserPostCard
+                user={{
+                  name: "Alex Johnson",
+                  avatar: "https://randomuser.me/api/portraits/men/32.jpg",
+                }}
+                content="Exploring the new React Native features today! 🔥 Can't wait to integrate them into my projects. #reactnative #devlife"
+                hashtags={["#reactnative", "#devlife"]}
+                timeAgo="2h ago"
+                likeCount={10}
+                commentCount={4}
+                onAvatarPress={handleAvatarPress}
+                onLikePress={handleLikePress}
+                onCommentPress={handleCommentPress}
+              />
+              </ScrollView>
+            </View>
       </ScrollView>
     </View>
   );
