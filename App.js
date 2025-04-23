d2fa0c1ae29776f27859f7097923aa19c11dc720
venkat2k25 <byvenkataraja@gmail.com>
2025-04-23 19:48:37 +0530

    safearea

--- App.js
+++ App.js
@@ -2,34 +2,37 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import AuthStack from './navigations/AuthStack';
 import MainTab from './navigations/MainTab';
 import ProfileStack from './navigations/ProfileStack';
-import NotificationScreen from './screens/NotificationScreen.jsx';
+import NotificationScreen from './screens/NotificationScreen.jsx'
 import LiveStack from './navigations/LiveStack.jsx';
 import CommunityStack from './navigations/CommunityStack.jsx';
+
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Auth" component={AuthStack} />
-          <Stack.Screen name="MainTab" component={MainTab} />
-          <Stack.Screen name="ProfileStack" component={ProfileStack} />
-          <Stack.Screen name="LiveStack" component={LiveStack} />
-          <Stack.Screen name="CommunityStack" component={CommunityStack} />
-          <Stack.Screen name="Notification" component={NotificationScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaView style={styles.safeArea} edges={['top', 'bottom', 'left', 'right']}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Auth" component={AuthStack} />
+            <Stack.Screen name="MainTab" component={MainTab} />
+            <Stack.Screen name="ProfileStack" component={ProfileStack} />
+            <Stack.Screen name="LiveStack" component={LiveStack} />
+            <Stack.Screen name="CommunityStack" component={CommunityStack} />
+            <Stack.Screen name="Notification" component={NotificationScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
 
 const styles = StyleSheet.create({
   safeArea: {
