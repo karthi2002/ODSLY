0fbddbb19496685177ae1659a94a94e38f1f738f
karthi srinivasan <94228520+karthi2002@users.noreply.github.com>
2025-04-24 16:08:08 +0530

    done

--- screens/community/CommunityScreen.jsx
+++ screens/community/CommunityScreen.jsx
@@ -7,10 +7,12 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Header from "../../layouts/Header";
+import Icon from "react-native-vector-icons/AntDesign";
 import { GradientText } from "../../components/Button/GradientText";
 import Colors from "../../utils/Colors";
 import FeedScreen from "./FeedScreen";
 import YourPostScreen from "./YourPostScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function CommunityScreen() {
 
@@ -64,6 +66,37 @@ export default function CommunityScreen() {
           )}
         </View>
       </ScrollView>
+
+{activeTab === "posts" && (
+  <>
+      {/* Fixed Button for Create new post */}
+      <TouchableOpacity
+        style={styles.fixedButton}
+        onPress={() =>
+          navigation.navigate("LiveStack", {
+            screen: "LiveNewBet",
+          })
+        }
+      >
+        <LinearGradient
+          colors={["#029EFE", "#6945E2", "#E9098E"]}
+          locations={[0, 0.37, 1]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.fixedButtonGradient}
+        >
+          <Icon
+            name="pluscircle"
+            size={20}
+            color={Colors.secondary}
+            style={{ marginRight: 8 }}
+          />
+          <Text style={styles.fixedButtonText}>Create Post</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+      </>
+      )}
+
     </View>
   );
 }
@@ -110,5 +143,27 @@ const styles = StyleSheet.create({
   },
   contentBox: {
     marginTop: 20,
-  }
+  },
+  fixedButton: {
+    position: "absolute",
+    bottom: 15,
+    right: -30,
+    alignItems: "center",
+    zIndex: 10,
+  },
+  fixedButtonGradient: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    borderRadius: 30,
+    alignItems: "center",
+    width: "60%",
+  },
+  fixedButtonText: {
+    color: Colors.secondary,
+    fontSize: 16,
+    fontWeight: "600",
+    marginRight: 8,
+  },
 });
