d026c581c90f1f2f224abe92a920c984ae1ecc1b
karthi srinivasan <94228520+karthi2002@users.noreply.github.com>
2025-04-25 13:27:31 +0530

    done

--- services/signup/googleSignup.js
+++ services/signup/googleSignup.js
@@ -2,30 +2,34 @@ import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import axios from 'axios';
 import { Alert } from 'react-native';
+import { BACKEND_URL } from '../../config/url';
 
 export const handleGoogleSignup = async (navigation) => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const { idToken } = await GoogleSignin.signIn();
-
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    const userCredential = await auth().signInWithCredential(googleCredential);
-    const user = userCredential.user;
-
-    const response = await axios.get(
-      `http://your-server-url/api/v1/googleSignup?email=${user.email}`
-    );
-
-    if (response.data.otpSent) {
-      Alert.alert("User Exists", "Redirecting to OTP verification.");
-      navigation.navigate('VerifyOTP', { email: user.email });
-    } else {
-      Alert.alert("New Signup", "Registered successfully.");
-      navigation.navigate('VerifyOTP', { email: user.email });
+    try {
+      await GoogleSignin.hasPlayServices();
+      const { idToken } = await GoogleSignin.signIn();
+  
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      const userCredential = await auth().signInWithCredential(googleCredential);
+      const user = userCredential.user;
+  
+      const response = await axios.get(
+        `${BACKEND_URL}/api/v1/googleSignup?email=${user.email}`
+      );
+  
+      const { otpSent, userExists } = response.data;
+  
+      if (userExists) {
+        Alert.alert("User Already Exists", "Redirecting to Sign In.");
+        navigation.navigate("SignIn", { email: user.email });
+      } else if (otpSent) {
+        Alert.alert("OTP Sent", "Please check your email.");
+        navigation.navigate("VerifyOTP", { email: user.email });
+      }
+  
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Authentication Error", "Google sign-in failed.");
     }
-
-  } catch (error) {
-    console.log(error);
-    Alert.alert("Authentication Error", "Google sign-in failed.");
-  }
-};
+  };
+  
