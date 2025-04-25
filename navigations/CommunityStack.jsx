2a1cf0e6b1c8c46ba1688e56ed8e7146949a85d5
madhan1204 <madhankumar1204@outlook.com>
2025-04-25 17:56:31 +0530

    New Post Card added

--- navigations/CommunityStack.jsx
+++ navigations/CommunityStack.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PostCommentScreen from '../screens/community/PostCommentScreen';
 import PostProfileScreen from '../screens/community/PostProfileScreen';
+import CreatePostScreen from '../screens/community/CreatePostScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function CommunityStack() {
       
       <Stack.Screen name="PostComment" component={PostCommentScreen} />
       <Stack.Screen name="PostProfile" component={PostProfileScreen} />
+      <Stack.Screen name="CreateNewPost" component={CreatePostScreen} />
 
     </Stack.Navigator>
   );
