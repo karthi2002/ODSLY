3925de3e4d08da1a22f6b2716b60b3eafc6a80d2
madhan1204 <madhankumar12002@gmail.com>
2025-06-15 15:59:01 +0530

    url updated

--- config/url.js
+++ config/url.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://192.168.6.159:3000"
+// export const BACKEND_URL = "http://192.168.164.159:3000"
 
-// export const BACKEND_URL = "https://odsly-backend.onrender.com"
+export const BACKEND_URL = "https://odsly-backend.onrender.com"
